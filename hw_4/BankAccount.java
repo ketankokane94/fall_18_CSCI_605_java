8d8362a4ae35ca564b9e5528dbe770d250ced583
ketankokane94 <ketankokane94@gmail.com>
2018-09-23 16:32:30 -0400

    commented the bank account file

--- hw_4/BankAccount.java
+++ hw_4/BankAccount.java
@@ -1,5 +1,16 @@
 import java.util.Random;
 
+/* 
+ * BankAccount.java 
+ * 
+ * Version: 
+ *     1
+ * 
+ * Revisions: 
+ *     1
+ */
+
+
 public abstract class BankAccount
 {
     private Random random = new Random();
@@ -7,7 +18,11 @@ public abstract class BankAccount
     AcountType accountType;
     String accountHolderName;
 
-
+/**
+ * constructor which creates a new account in the bank
+ * @param accountType SAVINGS CHECKING CREDIT CARD
+ * @param accountHolderName name of account hodler
+ */
     public BankAccount(AcountType accountType, String accountHolderName) {
         this.accountNo = generateNewAccountNumber();
         this.accountType = accountType;
@@ -15,6 +30,10 @@ public abstract class BankAccount
     }
 
     // todo: fix it to only 5 digits positive integers
+    /**
+     * returns a randomly generated account number
+     * @return
+     */
     private int generateNewAccountNumber() {
         return  random.nextInt()%100;
     }
