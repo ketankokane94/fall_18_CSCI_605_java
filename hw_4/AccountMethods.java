4ae23a6b736289dc45313010f113a5599e53e6c6
ketankokane94 <ketankokane94@gmail.com>
2018-09-23 16:39:10 -0400

    removed accountsMethod class

--- hw_4/AccountMethods.java
+++
@@ -1,9 +0,0 @@
-public interface AccountMethods
-{
-    public static void openAcc();
-    public static int findAccount(int accNum);
-    public static void closeAccount();
-    public static void creditAccount();
-    public static void debitAccount();
-    public static void printSummary();
-}
