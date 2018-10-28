cbf1914c7c8cc6223fb422599822ce523dce1a89
Ameya <ameya.nagnur18@gmail.com>
2018-10-28 13:27:51 -0400

    9.3 done!!!!

--- hw_9/prob9_3/ConsumerProducer.java
+++ hw_9/prob9_3/ConsumerProducer.java
@@ -108,28 +108,68 @@ public class ConsumerProducer extends Thread {
         System.out.println(" Done " + info);
     }
 
+    public static void areArgumentsNumbers(String argument0, String argument1, String argument2, String argument3, String argument4) throws NoNumberArgumentException {
+        try {
+            int a0 = Integer.parseInt(argument0);
+            int a1 = Integer.parseInt(argument1);
+            int a2 = Integer.parseInt(argument2);
+            int a3 = Integer.parseInt(argument3);
+            int a4 = Integer.parseInt(argument4);
+        }
+        catch (NumberFormatException notIntException) {
+            //notIntException.printStackTrace();
+            throw new NoNumberArgumentException("Argument is not a number");
+        }
+    }
+    public static void areArgumentsNegative(int argument0, int argument1, int argument2, int argument3, int argument4) throws NegativeNumberArgumentException {
+        if (argument0 < 0 || argument1 < 0 || argument2 < 0 || argument3 < 0 || argument4 < 0) {
+            throw new NegativeNumberArgumentException("Argument is negative");
+        }
+    }
+
     public static void main(String args[]) {
        // Grep.o = ""
-        int num_consumers;
-        int num_producers;
-        int num_produced;
-        int num_consumed;
+        int num_consumers = 2;
+        int num_producers = 2;
+        int num_produced = 4;
+        int num_consumed = 4;
 
         if (args.length < 5) {
             ConsumerProducer.storageSpace = 100;
             ConsumerProducer.storageSpaceLeft = 100;
-            num_consumers = 2;
-            num_producers = 2;
-            num_consumed = 4;
-            num_produced = 2;
         }
         else {
-            ConsumerProducer.storageSpace = 100;
-            ConsumerProducer.storageSpaceLeft = 100;
-            num_consumers = Integer.parseInt(args[0]);
-            num_producers = Integer.parseInt(args[1]);
-            num_consumed = Integer.parseInt(args[2]);
-            num_produced = Integer.parseInt(args[3]);
+            int argument0, argument1, argument2, argument3, argument4;
+
+            try {
+
+                // Throw exceptions
+                areArgumentsNumbers(args[0], args[1], args[2], args[3], args[4]);
+
+                argument0 = Integer.parseInt(args[0]);
+                argument1 = Integer.parseInt(args[1]);
+                argument2 = Integer.parseInt(args[2]);
+                argument3 = Integer.parseInt(args[3]);
+                argument4 = Integer.parseInt(args[4]);
+
+                areArgumentsNegative(argument0, argument1, argument2, argument3, argument4);
+
+                ConsumerProducer.storageSpace = argument4;
+                ConsumerProducer.storageSpaceLeft = argument4;
+                num_consumers = argument0;
+                num_producers = argument1;
+                num_consumed = argument2;
+                num_produced = argument3;
+
+            }
+            catch (NoNumberArgumentException noNumberArgumentException) {
+                noNumberArgumentException.printStackTrace();
+                System.exit(0);
+            }
+            catch (NegativeNumberArgumentException negativeNumberArgumentException) {
+                negativeNumberArgumentException.printStackTrace();
+                System.exit(0);
+            }
         }
 
         ConsumerProducer consumers[] = new ConsumerProducer[num_consumers];
