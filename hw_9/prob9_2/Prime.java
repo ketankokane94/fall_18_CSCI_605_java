ed5f79143af8642df6e395b9b5b694bc7f070a22
ketankokane94 <ketankokane94@gmail.com>
2018-10-28 07:10:59 -0400

    corrected the working of prime number logic

--- hw_9/prob9_2/Prime.java
+++ hw_9/prob9_2/Prime.java
@@ -3,53 +3,77 @@ import java.util.BitSet;
 class Prime extends Thread{
     int start, end;
 
-    public static BitSet notPrimeNumbers;
+    public static int notPrimeNumbers;
+    public static Object key;
+
     public Prime(int start, int end) {
         this.start = start;
         this.end = end;
     }
 
-
     public void run(){
+        int numberOfPrimes = 0;
         int limiter = end/2;
+        BitSet bits = new BitSet();
+
         for (int index = 2; index < limiter; index++) {
-            int multiplier = 2;
+            int multiplier = start/index < 2 ? 2: start/index;
             int number = index * multiplier;
             while (number <= end){
-            notPrimeNumbers.set(number);
+            bits.set(number);
                 multiplier++;
                 number = index * multiplier;
             }
         }
+        for (int i1 = start; i1 <= end; i1++) {
+            if (!bits.get(i1)){
+                numberOfPrimes++;
+            }
+        }
+        synchronized (key){
+            notPrimeNumbers+=numberOfPrimes;
+        }
     }
 
-    public static void main(String args[]){
-
+    public static void main(String args[]) throws InterruptedException {
+        key = new Object();
         int N=10000000;
-        notPrimeNumbers  = new BitSet(N);
+        notPrimeNumbers=0;
         long startTime = System.currentTimeMillis();
-        int start=2,end, numberOfThreads = 2;
-        end =  N/numberOfThreads;
+        int start = 2, numberOfThreads = 12;
+        int range =  N/numberOfThreads;
+        int end = range;
         Prime prime[] = new Prime[numberOfThreads];
         for (int index = 0; index < numberOfThreads-1; index++) {
-            new Prime(start,end).run();
+            prime[index] = new Prime(start,end);
+            prime[index].start();
             start = end + 1;
-            end = end + end;
+            end = end + range;
         }
-        start = end + 1;
-        end = N;
-        new Prime(start,end).run();
+        if (numberOfThreads!=1){
+            end = N;
+        }
+
+
+        prime[numberOfThreads-1] = new Prime(start,end);
+        prime[numberOfThreads-1].start();
+
+
+        for (int index = 0; index < numberOfThreads; index++) {
+            prime[index].join();
+        }
 
         long endTime = System.currentTimeMillis();
-        int count = 0;
+        //int count = 0;
+        /*
         for (int i1 = 2; i1 <= N; i1++) {
             if (!notPrimeNumbers.get(i1)){
                 count++;
             }
-        }
+        }*/
         //664,579 , 1379 sequential
         //664579 ,
-        System.out.println("number of prime numbers from " + count);
+        System.out.println("number of prime numbers from " + notPrimeNumbers);
         System.out.println("Took "+(endTime - startTime) + " ms");
     }
 }
