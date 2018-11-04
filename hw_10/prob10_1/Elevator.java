ccc274e7b8c16595f999372b2f0898ac280c4631
Ameya <ameya.nagnur18@gmail.com>
2018-11-04 14:35:00 -0500

    10_1

--- hw_10/prob10_1/Elevator.java
+++ hw_10/prob10_1/Elevator.java
@@ -15,10 +15,7 @@
 
 import java.util.Scanner;
 import java.io.*;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -30,11 +27,17 @@ public class Elevator extends Thread {
 
     static int storageSpace;
 
+    static int top_floor;
+
     int person_floor;
 
-    static boolean somePersonWaiting;
+    int destination_floor;
+
+    static int somePersonWaiting;
 
     static boolean elevatorWaiting;
+
+    static int num_persons[];
 
     static int elevator_floor;
 
@@ -46,12 +49,11 @@ public class Elevator extends Thread {
     /**
      * Constructor
      * @param info String with C or P indicating Consumer or Producer
-     * @param howMany number of items to produce or consume at a time
      */
-    public Elevator(String info, int floor) {
+    public Elevator(String info, int floor, int destination_floor) {
         this.info = info;
         elevatorWaiting = false;
-        somePersonWaiting = false;
+        somePersonWaiting = 0;
         if (info.contains("E")) {
             // Floor for start of elevator
             elevator_floor = floor;
@@ -60,7 +62,11 @@ public class Elevator extends Thread {
             // Floor on which person is or wants to go
             person_floor = floor;
             inElevator = false;
+
+            // Increment count for that floor
+            num_persons[floor]++;
         }
+        this.destination_floor = destination_floor;
         count = 0;
     }
 
@@ -70,6 +76,15 @@ public class Elevator extends Thread {
     public void exitElevator() {
         // Exit elevator
         count--;
+
+        // Swap destination and current floor for loop to go on with new results
+        int temp = destination_floor;
+        destination_floor = person_floor;
+        person_floor = temp;
+
+        inElevator = false;
+
+        num_persons[destination_floor]++;
 
         System.out.println(info + " exited elevator ");
     }
@@ -81,6 +96,10 @@ public class Elevator extends Thread {
         // Enter Elevator
         count++;
 
+        inElevator = true;
+
+        num_persons[person_floor]--;
+
         System.out.println(info + " entered elevator ");
     }
 
@@ -89,35 +108,55 @@ public class Elevator extends Thread {
      */
     public void run()
     {
-
+        int ctr = 0;
         // Loop to make process happen in tandem endlessly
-        while (true) {
+        while (ctr < 10) {
             synchronized (o) {
                 if (info.contains("P")) {
                     // if elevator is on the floor that person is on
-                    if (elevator_floor == person_floor) {
-                        // if there is space in the elevator
-                        if (!inElevator && count < storageSpace) {
-                            enterElevator();
-                        }
-                        else if (inElevator) {
-                            exitElevator();
-                        }
+                    // if there is space in the elevator
+                    if (elevator_floor == person_floor && !inElevator && count < storageSpace) {
+
+                        enterElevator();
+
+                    }
+                    else if (elevator_floor == destination_floor && inElevator) {
+                        exitElevator();
                     }
 
-                    if (elevatorWaiting) {
-                        o.notifyAll();
-                    }
+                    //if (elevatorWaiting) {
+                    //o.notifyAll();
+                    //System.out.println("Notified all");
+                    //}
                     try {
+                        //somePersonWaiting++;
                         o.wait();
                     } catch (InterruptedException ie) {
                         ie.printStackTrace();
+                        //somePersonWaiting--;
                     }
                 }
                 else if (info.contains("E")) {
-                    elevator_floor++;
+                    if (elevator_floor == top_floor) {
+                        elevator_floor--;
+                    }
+                    else {
+                        elevator_floor++;
+                    }
+                    System.out.println("Elevator is on floor " + elevator_floor);
+
                     o.notifyAll();
+
+                    try {
+                        elevatorWaiting = true;
+                        System.out.println("Elevator waiting for 1 second");
+                        o.wait(1000);
+                    } catch (InterruptedException ie) {
+                        ie.printStackTrace();
+                    }
+                    elevatorWaiting = false;
                 }
+                ctr++;
             }
         }
     }
@@ -126,24 +165,37 @@ public class Elevator extends Thread {
      * Main function.
      */
     public static void main(String args[]) {
+        int num_threads = 5;
+
         Elevator.storageSpace = 3;
-        ExecutorService executorService = new ThreadPoolExecutor(5, 10, 2000, null, null);
+        Elevator.top_floor = 3;
+        Elevator.num_persons =  new int[5];
+        ExecutorService executorService = Executors.newFixedThreadPool(num_threads);
         ThreadFactory threadFactory = ((ThreadPoolExecutor) executorService).getThreadFactory();
 
-        for (int i  = 0; i < 10; i++) {
+        for (int i  = 0; i < num_threads; i++) {
             String information;
             int floor;
+            int destination_floor;
             if (i == 0) {
                 information = "E";
                 floor = 0;
+                destination_floor = 0;
             }
             else {
                 information = "P" + i;
-                floor = ((int)(Math.random() * 10000)) % 3;
+                floor = ((int)(Math.random() * 10000)) % 4;
+                if (floor != 0) {
+                    destination_floor = 0;
+                }
+                else {
+                    destination_floor = 3;
+                }
             }
-            executorService.execute(threadFactory.newThread(new Elevator(information, floor)));
+            executorService.execute(threadFactory.newThread(new Elevator(information, floor, destination_floor)));
+            System.out.println("Created " + information + " with floor " + floor);
         }
-
+        executorService.shutdown();
 
     }
 }
