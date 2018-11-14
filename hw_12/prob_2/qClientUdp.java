280770dd15c85f9b1e47f2ee0d1807958320cadc
ketankokane94 <ketankokane94@gmail.com>
2018-11-14 13:39:14 -0500

    Added the quote file to randomly return a quote when a client connect

--- hw_12/prob_2/qClientUdp.java
+++ hw_12/prob_2/qClientUdp.java
@@ -7,18 +7,28 @@ import java.net.UnknownHostException;
 
 public class qClientUdp extends Thread{
 
+    Socket socket;
 
-    public qClientUdp(int port){
-
+    public qClientUdp(int port) throws IOException {
+             socket = new Socket(InetAddress.getLocalHost(),port);
     }
-
-    public static void main(String args[]){
+    public void run(){
         try {
-            Socket socket = new Socket(InetAddress.getLocalHost(),12345);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             System.out.println(bufferedReader.readLine());
             socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            socket = null;
+        }
 
+    }
+    public static void main(String args[]){
+        try {
+            qClientUdp clientUdp = new qClientUdp(12345);
+            clientUdp.start();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
--- hw_12/prob_2/qServerUdp.java
+++ hw_12/prob_2/qServerUdp.java
@@ -1,13 +1,18 @@
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.awt.desktop.QuitEvent;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class qServerUdp extends Thread{
     ServerSocket server;
+    List<String> Quotes ;
 
     public qServerUdp(int portNumber) throws IOException {
         server = new ServerSocket(portNumber);
+        read();
         System.out.println("server started listening on "+server.getInetAddress() + " on port " + server.getLocalPort());
 
     }
@@ -15,10 +20,12 @@ public class qServerUdp extends Thread{
     public void run(){
         try {
             while (true) {
+
                 Socket client = server.accept();
                 System.out.println("a new client connected to the server");
                 PrintWriter out = new PrintWriter(client.getOutputStream());
-                out.println("Hello you connected to the server");
+                out.println(Quotes.get(new Random().nextInt(Quotes.size())));
+                System.out.println(Quotes.get(((int)Math.random()) % Quotes.size()));
                 out.close();
             }
 
@@ -27,8 +34,34 @@ public class qServerUdp extends Thread{
         }
 
     }
+
+    private void read() throws IOException {
+        Quotes = new ArrayList<>();
+        File file = new File(new File("").getCanonicalPath() + "/data.txt");
+        String line ;
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            StringBuilder sb = new StringBuilder();
+            while ((line = bufferedReader.readLine())!= null){
+                line = line.trim();
+                    if (line.equals("%%")){
+                        if (!sb.toString().equals(""))
+                             Quotes.add(sb.toString());
+                        sb = new StringBuilder();
+                    }
+                    else {
+                        sb.append(line);
+                    }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String args[]){
-        int port = 12345;
         try {
             qServerUdp serverUdp = new qServerUdp(12345);
             serverUdp.start();
